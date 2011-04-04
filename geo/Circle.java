c156409438f84f77bea43c0d4777bd9f5762c3d1
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-04-04 09:10:19 -0700

    Se refactorizo Cicle para mostrar Constructores, setters, getters y metodos privados

--- geo/Circle.java
+++ geo/Circle.java
@@ -1,4 +1,49 @@
 public class Circle {
+	double radio = 0;
+	double pi = 3.1416;
+	
+	public Circle() {
+		System.out.println("Inicializando objecto de tipo Circle");
+	}
+	
+	public Circle(double radio) {
+		System.out.println("Inicializando con radio " + radio);
+		this.radio = radio;
+	}
+	
+	public double area() {
+		return pi * radio * radio;
+	}
+	
+	public double diameter() {
+		return radio + radio;
+	}
+	
+	// Setters y Getters
+	public double getRadio() {
+		return radio;
+	}
+	
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	
+	public double getPi() {
+		return pi;
+	}
+	
+	public String toString() {
+		return buildMessage();
+	}
+	
+	private String buildMessage() {
+		String message = "Circle\n" +
+		"radio: " + radio + "\n" +
+		"pi: " + pi;
+		
+		return message;
+	}
+	
 	public static double calculaArea(double pi, double r)
 	{
 		return pi * r * r;
--- geo/Geo.java
+++ geo/Geo.java
@@ -11,5 +11,19 @@ public class Geo {
 		
 		a = Box.calculaArea(l);
 		System.out.println(a);
+		
+		Circle c1 = new Circle(3.4);
+		System.out.println("Area de mi objeto Circle es " + c1.area());
+		
+		Circle c2 = new Circle(7.6);
+		System.out.println("Area de mi objeto Circle es " + c2.area());
+		
+		System.out.println(c1.getRadio());
+		System.out.println(c2.getRadio());
+		
+		c1.setRadio(5.8);
+		System.out.println("Area de mi objeto Circle es " + c1.area());
+		
+		System.out.println(c2.toString());
 	}
 }
