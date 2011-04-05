3965ee43c641c71b73d93c0ae3533933d44c462e
Mario Alberto Chavez <mario.chavez@gmail.com>
2011-04-05 09:31:42 -0700

    Ejemplo de exceptiones en Java

--- collections/Ejemplo.java
+++ collections/Ejemplo.java
@@ -1,22 +1,11 @@
+import java.io.*;
+
 public class Ejemplo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		//primitiveCast();
-		
-		Client client = new Client();
-		client.setName("Sperantus");
-		client.setAddress("Tijuana");
-		client.setClassification("A");
-		
-		print(client.toString());
-		
-		Company company = (Company)client;
-		print(company.toString());
-		
-		Company company1 = new Client();
-		company1.setName("Sperantus1");
-		company1.setAddress("Tijuana1");
-		((Client)company1).setClassification("B");
-		print(company1.toString());
+		//classCast();
+		//exceptions();
+
 	}
 	
 	public static void print(Object data) {
@@ -35,5 +24,41 @@ public class Ejemplo {
 		x = Double.parseDouble(z);
 		
 		print(z);
+	}
+	
+	public static void classCast() {
+		Client client = new Client();
+		client.setName("Sperantus");
+		client.setAddress("Tijuana");
+		client.setClassification("A");
+		
+		print(client.toString());
+		
+		Company company = (Company)client;
+		print(company.toString());
+		
+		Company company1 = new Client();
+		company1.setName("Sperantus1");
+		company1.setAddress("Tijuana1");
+		((Client)company1).setClassification("B");
+		print(company1.toString());
+	}
+	
+	public static void exceptions() {
+		int x = 3;
+		int y = 0;
+		
+		try {
+			y = x/0;
+		} catch(ArithmeticException e) {
+			print(e.toString());
+		}
+		finally {
+			print("Todo bien");
+		}
+		
+		throw new FileNotFoundException("El archivo readme.txt no fue encontrado");
+		
+		//print(y);
 	}
 }
